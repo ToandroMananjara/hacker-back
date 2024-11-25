f210df63157e6a6db665f25c9813fd6454afc69a
Toandro <toandro@Toandros-MacBook-Pro.local>
2024-11-25 15:22:53 +0300

    siganlement mandeha

--- controller/signalementController.php
+++ controller/signalementController.php
@@ -70,11 +70,24 @@ class SignalementController
                     mkdir($uploadDir, 0777, true);  // Crée le dossier avec les permissions appropriées
                 }
 
+                // echo json_encode([
+                //     'status' => 'success',
+                //     'message' => 'Plainte placé avec succès.',
+                //     $user_id = $_POST['user_id'],
+                //     $full_name = $_POST['full_name'],
+                //     $date = $_POST['date'],
+                //     $hour = $_POST['hour'],
+                //     $location = $_POST['location'],
+                //     $description = $_POST['description'],
+                //     $file = $_FILES['file_path']
+
+                // ], JSON_UNESCAPED_UNICODE);
+
                 $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
                 $newFileName = uniqid() . '.' . $fileExtension;  // Nouveau nom de fichier unique
                 $destination = $uploadDir . '/' . $newFileName;
 
-                // Déplacer le fichier téléchargé vers le dossier cible
+                // // Déplacer le fichier téléchargé vers le dossier cible
                 if (!move_uploaded_file($fileTmpPath, $destination)) {
                     throw new Exception("Une erreur est survenue lors de l'enregistrement de la photo de profil.");
                 }
@@ -94,6 +107,8 @@ class SignalementController
                 $isCreate = $signalements->createReport();
 
                 if ($isCreate) {
+                    http_response_code(200);
+
                     echo json_encode([
                         'status' => 'success',
                         'message' => 'Plainte placé avec succès.',
@@ -103,6 +118,7 @@ class SignalementController
                         'status' => 'failed',
                         'message' => 'Erreur lors de la soumission de formulaire',
                     ], JSON_UNESCAPED_UNICODE);
+                    http_response_code(200);
                 }
                 // Assurez-vous que l'ID de l'utilisateur est transmis, probablement via un token ou une session
 
@@ -110,6 +126,7 @@ class SignalementController
                 // Répondre avec un message de succès
 
             } catch (Exception $e) {
+
                 // Gérer les erreurs
                 http_response_code(400);
                 echo json_encode(['status' => 'failed', 'error' => 'Bad request', 'message' => $e->getMessage()], JSON_UNESCAPED_UNICODE);
